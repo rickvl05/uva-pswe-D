9f29247855337f5ba03419752a4cf5b7d51e2acd
Max Boot <maxfboot@gmail.com>
2024-06-11 13:07:16 +0200

    Throwing fixed and new player sprite

--- PuzzlePlatformer/scripts/items/Crate.gd
+++ PuzzlePlatformer/scripts/items/Crate.gd
@@ -1,17 +1,12 @@
 extends RigidBody2D
 
-@export var held_by: CharacterBody2D = null
+var held_by: CharacterBody2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	freeze_mode = RigidBody2D.FREEZE_MODE_KINEMATIC
 	if !multiplayer.is_server():
 		freeze = true
-
-func _process(delta):
-	if multiplayer.is_server():
-		if held_by != null:
-			# Necessary to keep triggering integrate forces
-			apply_central_force(Vector2(1,1))
 
 func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 	if multiplayer.is_server():
@@ -19,3 +14,4 @@ func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
 			# Position has to be updated in this function otherwise it will interfere
 			# with the physics simulation
 			position = held_by.global_position + Vector2(0, -held_by.item_height)
+			rotation = 0
