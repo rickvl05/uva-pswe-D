4290819d5da8bdbfd6869b06a8557c462d6766af
ikygb <github.ikygb@simplelogin.co>
2024-06-11 14:37:49 +0200

    wip(chat): show message on player

--- PuzzlePlatformer/scripts/chat.gd
+++ PuzzlePlatformer/scripts/chat.gd
@@ -1,27 +1,44 @@
 extends LineEdit
 
+@export var GameScene: Node:
+	set(node):
+		GameScene = node
+
 
 func _on_ready():
-	#hide()
+	hide()
 	pass
 
+
 func _on_focus_exited():
-	#hide()
+	hide()
 	pass
 
 
 func _input(event):
 	if event.is_action_pressed("type_message"):
-		#show()
+		show()
 		#grab_focus.call_deferred()
 		#grab_focus()
-		grab_click_focus()
+		#grab_click_focus()
 
 
 # Print the given message to Godot's logger.
 @rpc("call_local", "any_peer", "reliable")
 func print_message(message: String):
 	print(message)
+
+	var sender_id: int = multiplayer.get_remote_sender_id()
+
+	print(GameScene)
+
+	var players = GameScene.get_node("Players")
+	print(players)
+
+	var player = players.get_node(str(sender_id))
+	print(player)
+
+	player.get_node("SpeechBox").text = message
 
 
 # Broadcast the given message to all players.
@@ -40,6 +57,3 @@ func broadcast_default_message():
 	const message: String = "Hello, world!"
 
 	broadcast_message(message)
-
-
-
--- PuzzlePlatformer/scripts/game.gd
+++ PuzzlePlatformer/scripts/game.gd
@@ -3,3 +3,5 @@ extends Node2D
 
 func _enter_tree():
 	MultiplayerManager.set("GameScene", self)
+	Chat.set("GameScene", self)
+	print(Chat.GameScene)
