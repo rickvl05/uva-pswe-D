f1db636fdd0298b2dff480750c60ee870d69b3ba
MiniFlighter <victor@vlugten.nl>
2024-06-11 13:53:11 +0200

    Remove old project files

--- keybinds.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-
-func _on_button_pressed():
-	Click.play()
-	get_tree().change_scene_to_file("res://scenes/settings.tscn")
--- scenes/level_editor.gd
+++
@@ -1,8 +0,0 @@
-extends Node2D
-
-
-
-
-func _on_button_pressed():
-	Click.play()
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
