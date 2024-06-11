234b0c02d06dea43e25b025f3a22a576d88a5fbc
MiniFlighter <victor@vlugten.nl>
2024-06-11 12:08:59 +0200

    fixed broken crate

--- PuzzlePlatformer/scripts/items/crate.gd
+++ PuzzlePlatformer/scripts/items/crate.gd
@@ -1,17 +1,21 @@
 extends RigidBody2D
 
-@export var bounce_strength = 500
-
+@export var held_by: CharacterBody2D = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if !multiplayer.is_server():
+		freeze = true
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if multiplayer.is_server():
+		if held_by != null:
+			# Necessary to keep triggering integrate forces
+			apply_central_force(Vector2(1,1))
 
-func _on_bouncepad_body_entered(body):
-	if body is Player:
-		body.velocity.y = -bounce_strength
+func _integrate_forces(state: PhysicsDirectBodyState2D) -> void:
+	if multiplayer.is_server():
+		if held_by != null:
+			# Position has to be updated in this function otherwise it will interfere
+			# with the physics simulation
+			position = held_by.global_position + Vector2(0, -held_by.item_height)
