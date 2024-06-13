c86d3e14cacf3e844b64d1041d0f260bc1576a5c
rcg270 <robbychingjong@gmail.com>
2024-06-13 13:41:14 +0200

    mouse functinoality added again

--- TileMapEditornew/scripts/Editor_Object.gd
+++ TileMapEditornew/scripts/Editor_Object.gd
@@ -14,6 +14,7 @@ var is_panning: bool = false
 @export var max_zoom: float = 2.0
 @export var min_zoom: float = 0.5
 @export var current_item: PackedScene = null
+@export var current_rect: TextureRect = null
 @export var current_tile_id: Vector2i
 @export var selected_tile: bool = false
 
@@ -25,7 +26,7 @@ var current_item_index = 0
 var previous_item = null
 
 func _ready() -> void:
-	print("level=",level)
+	print("level=", level)
 	editor_cam.make_current()
 	pass
 
@@ -33,54 +34,74 @@ func _process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
 	
 	if !Global.playing:
-		move_tab()
+		handle_editor(global_position)
 	else:
-		move_editor()
 		if Input.is_action_just_pressed("mb_right"):
 			is_panning = true
 		elif Input.is_action_just_released("mb_right"):
 			is_panning = false
 
+func handle_editor(global_position):
+	#print("item:", item)
+	#print("curitem:", current_item)
+	#print("newitem: ", new_item, new_item.get("tile"))
+	if (current_item != null and can_place and Input.is_action_just_pressed("mb_left")):
+		print(current_item, can_place)
+		select_item(current_item)
+	elif (current_rect != null and can_place and Input.is_action_just_pressed("mb_left")):
+		print("haa")
+		select_tile(current_rect)
+	#var new_item = current_item.instantiate()
+	#level.add_child(new_item)
+		#new_item.global_position = get_global_mouse_position()
+	pass
+		
+
 func move_tab():
+	pass
 	if navigation_mode == NavigationMode.NAVIGATE_TABS:
 		navigate_tabs()
 	else:
 		navigate_items()
 
 func navigate_tabs():
+	pass
 	var tab_count = tab_container.get_tab_count()
 	var current_tab = tab_container.current_tab
 
-	if Input.is_action_just_pressed("ui_up") or Input.is_action_just_pressed("ui_left"):
+	# Mouse wheel to navigate tabs
+	if Input.is_action_just_pressed("mb_scroll_up"):
 		current_tab -= 1
-	elif Input.is_action_just_pressed("ui_down") or Input.is_action_just_pressed("ui_right"):
+	elif Input.is_action_just_pressed("mb_scroll_down"):
 		current_tab += 1
 
 	current_tab = clamp(current_tab, 0, tab_count - 1)
 	tab_container.current_tab = current_tab
 
-	if Input.is_action_just_pressed("ui_accept"): # Assuming 'Enter' is mapped to 'ui_accept'
+	if Input.is_action_just_pressed("mb_left"): # Left mouse button for selecting tab
 		navigation_mode = NavigationMode.NAVIGATE_ITEMS
 		current_item_index = 0 # Reset item index when switching to item navigation
 		highlight_item()
 
 func navigate_items():
+	pass
 	var current_tab_index = tab_container.current_tab
 	var current_vbox = tab_container.get_child(current_tab_index).get_node("ScrollContainer/VBoxContainer")
 	var current_hbox = current_vbox.get_child(0) # Assuming there's only one HBoxContainer per tab
 	var item_count = current_hbox.get_child_count()
 
-	if Input.is_action_just_pressed("ui_right"):
+	# Mouse wheel to navigate items
+	if Input.is_action_just_pressed("mb_scroll_down"):
 		current_item_index += 1
 		highlight_item()
-	elif Input.is_action_just_pressed("ui_left"):
+	elif Input.is_action_just_pressed("mb_scroll_up"):
 		if current_item_index > 0:
 			current_item_index -= 1
 		highlight_item()
 
 	current_item_index = clamp(current_item_index, 0, item_count - 1)
 
-	if Input.is_action_just_pressed("ui_accept"): # Assuming 'Enter' is mapped to 'ui_accept'
+	if Input.is_action_just_pressed("mb_left"): # Left mouse button to select item
 		var selected_item = current_hbox.get_child(current_item_index)
 		var IsTile = selected_item.get("tile")
 		if IsTile == false:
@@ -90,11 +111,12 @@ func navigate_items():
 		else:
 			print("error: object is neither item nor tile")
 
-	if Input.is_action_just_pressed("ui_cancel"): # Assuming 'Esc' is mapped to 'ui_cancel' for going back to tab navigation
+	if Input.is_action_just_pressed("mb_right"): # Right mouse button to go back to tab navigation
 		navigation_mode = NavigationMode.NAVIGATE_TABS
 		remove_highlight()
 
 func highlight_item():
+	pass
 	if previous_item:
 		remove_highlight()
 	var current_tab_index = tab_container.current_tab
@@ -105,22 +127,15 @@ func highlight_item():
 	previous_item = item
 
 func remove_highlight():
+	pass
 	if previous_item:
 		previous_item.modulate = Color(1, 1, 1) # Original color
 		previous_item = null
 
 func select_item(item):
-	print("select item")
-	print(item)
-	print(can_place)
 	if item and can_place:
-		print("selected item ", item)
-		print("can place: ", can_place)
-		print("playing: ", Global.playing)
-		print(item.get("this_scene"))
 		if item.has_method("get"):
 			var scene = item.get("this_scene")
-			print("Scene is:", scene)
 			if scene:
 				current_item = scene  # Update the current_item here
 				selected_tile = false
@@ -140,21 +155,6 @@ func select_tile(tile):
 			current_item = tile.get("this_scene")
 			selected_tile = true
 
-func move_editor() -> void:
-	#if !Global.playing:
-		#pass
-	var move_vector: Vector2 = Vector2.ZERO
-	if Input.is_action_pressed("ui_up"):
-		move_vector.y -= cam_spd
-	if Input.is_action_pressed("ui_left"):
-		move_vector.x -= cam_spd
-	if Input.is_action_pressed("ui_down"):
-		move_vector.y += cam_spd
-	if Input.is_action_pressed("ui_right"):
-		move_vector.x += cam_spd
-	#emit_signal("move_editor_finished")
-	editor.global_position += move_vector * get_process_delta_time()
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
 		
@@ -170,7 +170,6 @@ func _unhandled_input(event: InputEvent) -> void:
 		if is_panning:
 			editor.global_position -= event.relative * editor_cam.zoom
 
-
 func place_tile(tile_id):
 	var mousepos = tile_map.world_to_map(get_global_mouse_position())
 	tile_map.set_cell(0, Vector2i(mousepos.x, mousepos.y), Global.current_tile)
@@ -178,4 +177,3 @@ func place_tile(tile_id):
 func remove_tile():
 	var mousepos = tile_map.world_to_map(get_global_mouse_position())
 	tile_map.set_cell(0, Vector2i(mousepos.x, mousepos.y), -1)
-
