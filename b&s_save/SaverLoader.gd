9db50570f3ba683fc721ef699aa77fb815314be0
Samuel <samuel-ampadu@outlook.com>
2024-06-14 14:04:46 +0200

    Added saveloader script to b&s map for Gijs

--- b&s_save/SaverLoader.gd
+++ b&s_save/SaverLoader.gd
@@ -2,7 +2,12 @@ class_name SaverLoader
 extends Node
 #@onready var tile_map = %TileMap 
 @onready var tile_map = $"../../World/TileMap"
-
+var packed_scenes = {
+	"Door": preload("res://objects/door.tscn"),
+	"Entity": preload("res://objects/entity.tscn"),
+	#"TileGrass": preload("res://objects/tile_grass.tscn"),
+	"King": preload("res://objects/king.tscn")
+}
 
 func _on_save_but_pressed():
 	var file = FileAccess.open("user://savegame.json", FileAccess.WRITE)
@@ -26,19 +31,9 @@ func _on_load_but_pressed():
 	if file.file_exists("user://savegame.json"):
 		var json_text = file.get_as_text()
 		var saved_data = JSON.parse_string(json_text)
-		print(saved_data)
-		if saved_data:
-			#saved_data = saved_data.result
-			print("Komt hij hier")
-			tile_map.clear_items()
-				
-			for item in saved_data["items"]:
-				var position = Vector2(floor(item["position:x"]), floor(item["position:y"]))
-				var item_type = item["type"]
-				tile_map.place_item(item)
-				print("Dit is hier", item)
-		else:
-			print("Failed", saved_data.error_string)
+		for item in saved_data["items"]:
+			var position = Vector2(item["position:x"], item["position:y"])
+			var type = item["type"]
+			tile_map.placed_items[position] = type
+			print("Dit zijn de items", tile_map.placed_items[position])
 		file.close()
-	else:
-		print("save file not found")
