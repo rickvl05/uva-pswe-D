944a0dea9eddbc4975c39cfe6bf49ae3442a8c7a
Max Boot <maxfboot@gmail.com>
2024-06-20 14:44:33 +0200

    Moving platform synchronized

--- PuzzlePlatformer/scripts/items/platform.gd
+++ PuzzlePlatformer/scripts/items/platform.gd
@@ -1,26 +1,28 @@
 extends AnimatableBody2D
 
-# standonplatform is an area2d
+@export var players_needed: int
+
 @onready var standonplatform = $Standonplatform
 @onready var playeramounttext = $Playeramount
 @onready var platform_animator = $PlatformAnimator
 
-var playeramount = 0
+var playercount = 0
+
+func _ready():
+	playeramounttext.text = str(playercount) + " / " + str(players_needed) + " Players"
 
 func _on_standonplatform_body_entered(_body):
-	playeramount += 1
-	playeramounttext.text = str(playeramount) + " / 2 Players"
+	playercount += 1
+	playeramounttext.text = str(playercount) + " / " + str(players_needed) + " Players"
 #
-	if playeramount >= 2:
+	if playercount == players_needed and multiplayer.is_server():
 		# set custom speed to positive
 		platform_animator.play("going up", -1, 1.0, false)
 
 
 func _on_standonplatform_body_exited(_body):
-	playeramount -= 1
-	playeramounttext.text = str(playeramount) + " / 2 Players"
-	if playeramount < 2:
+	playercount -= 1
+	playeramounttext.text = str(playercount) + " / " + str(players_needed) + " Players"
+	if playercount == players_needed - 1 and multiplayer.is_server():
 		# set custom speed to negative
-		platform_animator.play("going up", -1, -1.0, false)
-	
-
+		platform_animator.play("going up", -1, -1.0, true)
