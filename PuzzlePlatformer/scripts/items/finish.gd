bba741bca7d7030b1ee92e2e27524974b5c9c25c
Jubin2004 <14587386@uva.nl>
2024-06-20 13:17:33 +0200

    level 2 and 3

--- PuzzlePlatformer/scripts/items/finish.gd
+++ PuzzlePlatformer/scripts/items/finish.gd
@@ -1,11 +1,9 @@
 extends Area2D
 
 
-@export var next_level_number = 1
+@export var next_level_number = 2
 
 
 func _on_body_entered(body) -> void:
 	if body.is_in_group("Player"):
 		get_tree().root.get_node("Game").change_level(next_level_number)
-		# tp player to 25, 50
-		body.global_position = Vector2(25, 50)
