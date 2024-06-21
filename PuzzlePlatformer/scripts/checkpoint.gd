21eed3a36fe7219cb8c596baffc70fffe1f29244
MiniFlighter <victor@vlugten.nl>
2024-06-21 13:53:18 +0200

    Added checkpoint pop animation

--- PuzzlePlatformer/scripts/checkpoint.gd
+++ PuzzlePlatformer/scripts/checkpoint.gd
@@ -2,6 +2,16 @@ extends Area2D
 
 
 func _on_body_entered(body):
+	assert($AnimatedSprite2D, "This function shouldn't be called if the checkpoint has already been reached")
 	if body.is_in_group("Player"):
 		var checkpoint = Vector2(global_position.x, global_position.y - 25)
 		body.set_checkpoint(checkpoint)
+		if $AnimatedSprite2D.animation == "default":
+			$AnimatedSprite2D.play("pop")
+
+
+func _on_animated_sprite_2d_animation_finished():
+	if $AnimatedSprite2D.animation == "pop":
+		hide()
+		$CollisionShape2D.disabled = true
+		$AnimatedSprite2D.queue_free()
