270c56cff1c6265759faad8e07c53b346a503f7b
MiniFlighter <victor@vlugten.nl>
2024-06-21 14:19:18 +0200

    Only the local player can trigger the checkpoint

--- PuzzlePlatformer/scripts/checkpoint.gd
+++ PuzzlePlatformer/scripts/checkpoint.gd
@@ -4,6 +4,8 @@ extends Area2D
 func _on_body_entered(body):
 	assert($AnimatedSprite2D, "This function shouldn't be called if the checkpoint has already been reached")
 	if body.is_in_group("Player"):
+		if body.name != str(multiplayer.get_unique_id()):
+			return
 		var checkpoint = Vector2(global_position.x, global_position.y - 25)
 		body.set_checkpoint(checkpoint)
 		if $AnimatedSprite2D.animation == "default":
