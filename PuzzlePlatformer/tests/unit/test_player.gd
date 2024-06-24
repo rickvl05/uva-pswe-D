6d5e4421a36fca318af5527b7063935080eb94eb
rickvl05 <rick@bumbal.eu>
2024-06-24 21:29:43 +0200

    Added github action

--- PuzzlePlatformer/tests/unit/test_player.gd
+++ PuzzlePlatformer/tests/unit/test_player.gd
@@ -1,11 +1,13 @@
 extends GutTest
 
-var player_scene = load('res://scenes/player.tscn')
-var player = null
-var sender = InputSender.new(Input)
+var player: Player = null
+var sender = null
 
 func before_each():
-	player = add_child_autofree(player_scene.instantiate())
+	get_tree().root.add_child(load("res://scenes/menus/main_menu.tscn").instantiate())
+	MultiplayerManager.host_game()
+	player = get_tree().get_nodes_in_group("Player")[0]
+	sender = InputSender.new(player)
 	
 func after_each():
 	sender.release_all()
@@ -27,9 +29,22 @@ func test_set_checkpoint():
 	assert_eq(player.spawn_point, spawn_point)
 
 func test_respawn():
-	var spawn_point = Vector2(5, 5)
+	var spawn_point = Vector2(41, 100)
 	player.set_checkpoint(spawn_point)
 	player.respawn()
 	
-	assert_eq(player.position, spawn_point)
+	assert_eq(player.global_position, spawn_point)
+
+func test_move_right():
+	var old_pos = player.position.x
+	sender.action_down("move_right").hold_for('5f')
+	await(sender.idle)
 	
+	assert_gt(player.position.x, old_pos)
+	
+func test_move_left():
+	var old_pos = player.position.x
+	sender.action_down("move_left").hold_for('5f')
+	await(sender.idle)
+	
+	assert_lt(player.position.x, old_pos)
