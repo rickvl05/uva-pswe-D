76c0a11f77f75ffe5cfe7eeeab63c6afe1ab5f9a
rickvl05 <rick@bumbal.eu>
2024-06-26 09:40:05 +0200

    added item tests

--- PuzzlePlatformer/tests/unit/test_multiplayer.gd
+++ PuzzlePlatformer/tests/unit/test_multiplayer.gd
@@ -1,22 +1,20 @@
 extends GutTest
 
-#var manager = null
+var manager = null
 
-func test_test():
-	assert_eq(1,1)
-
-#func before_each():
-	#manager = MultiplayerManager
-	#
-#func test_initial_state():
-	#assert_eq(manager.available_colors, [1,2,3,4])
-	#assert_eq(manager.player_count, 0)
-	#assert_eq(manager.GameScene, null)
-	#
-#func test_host_game():
-	#manager.host_game()
-	#
-	#assert_eq(manager.player_count, 1)
-	#
-#func test_join_game_error():
-	#assert_eq(manager.join_game(), 0)
+func before_each():
+	get_tree().root.add_child(load("res://scenes/menus/main_menu.tscn").instantiate())
+	manager = MultiplayerManager
+	
+func test_initial_state():
+	assert_eq(manager.available_colors, [1,2,3,4])
+	assert_eq(manager.player_count, 0)
+	assert_eq(manager.GameScene, null)
+	
+func test_host_game():
+	manager.host_game()
+	
+	assert_eq(manager.player_count, 1)
+	
+func test_join_game_error():
+	assert_eq(manager.join_game(), 0)
